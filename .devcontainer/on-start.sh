69fccf5dd6cb228309bffd2abea9337d684025ea
Simon Prinz <simon.prinz@live.de>
2023-08-19 00:26:35 +0200

    tests

---
+++ .devcontainer/on-start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Welcome to your Shopware Workspace!"
+echo ""
+echo "You can get started by starting the MariaDB-Server and the Apache2 by running:"
+echo " - service mariadb start"
+echo " - service apache2 start"
